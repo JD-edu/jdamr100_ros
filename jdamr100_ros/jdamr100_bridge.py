d14c054576eb34a1f0209a421de4ed485a3bed4c
JD-Edu <jdedu.kr@gmail.com>
2025-02-19 23:13:36 +0900

    Feb 19th

--- jdamr100_ros/jdamr100_bridge.py
+++ jdamr100_ros/jdamr100_bridge.py
@@ -6,16 +6,12 @@ from std_msgs.msg import Int64, String
 class Jdamr100Bridge(Node):
     def __init__(self):
         super().__init__('jdamr100_bridge')
-        
-        #self.subscription_linear = self.create_subscription(Int64, 'linear', self.linear_callback, 10)
-        #self.subscription_angular = self.create_subscription(Int64, 'angular', self.angular_callback, 10)
+     
         self.subscription_keyinput = self.create_subscription(String, 'key', self.key_callback, 10)
        
         self.host = '172.30.1.38'
         self.port = 8080
         
-        self.min_vel = 0
-        self.max_vel = 250
         # esp32 connect 
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -24,39 +20,9 @@ class Jdamr100Bridge(Node):
         except socket.error as e:
             self.get_logger().error(f"Failed to con-nect to server: {e}")
 
-        self.linear_velocity = 0
-        self.angular_velocity = 0
-        
-        #self.timer = self.create_timer(0.5, self.send_motor_control)
-
-    def linear_callback(self, msg):
-        self.linear_velocity = msg.data
-
-    def angular_callback(self, msg):
-        self.angular_velocity = msg.data
-
     def key_callback(self, msg):
         print(msg.data)
         self.client.send(msg.data.encode())
-
-    def send_motor_control(self):
-        try:
-            motor_control = self.motor_speed_vel(self.linear_velocity, self.angular_velocity)
-            self.client.send(motor_control.encode())
-            self.get_logger().info(f'Sent data: {motor_control}')
-        except socket.error as e:
-            self.get_logger().error(f"Failed to send data: {e}")
-    
-    def motor_speed_vel(self, linear, angular):
-        vel_R = linear + angular
-        vel_L = linear - angular
-        
-        motor_R = constrain(vel_R, self.min_vel, self.max_vel)
-        motor_L = constrain(vel_L, self.min_vel, self.max_vel)
-        motor = f"1a{motor_R}b{motor_L}c\n"        
-        
-        self.get_logger().info(f'motor_R: {motor_R}, motor_L: {motor_L}')
-        return motor
 
 def constrain(value, min_value, max_value):
     return max(min(value, max_value), min_value)
@@ -70,11 +36,9 @@ def main(args=None):
     except KeyboardInterrupt:
         cmd_vel_subscriber.get_logger().info("Shutting down...")
     finally:
-        cmd_vel_subscriber.linear_velocity = 0
-        cmd_vel_subscriber.angular_velocity = 0
         motor_control = cmd_vel_subscriber.motor_speed_vel(0, 0)
         try:
-            cmd_vel_subscriber.client.send(motor_control.encode())
+            cmd_vel_subscriber.client.send(' \n'.encode())
             cmd_vel_subscriber.get_logger().info(f'Sent stop command: {motor_control}')
         except socket.error as e:
             cmd_vel_subscriber.get_logger().error(f"Failed to send stop command: {e}")
--- jdamr100_ros/jdamr100_teleop.py
+++ jdamr100_ros/jdamr100_teleop.py
@@ -11,15 +11,6 @@ import tty
 from std_msgs.msg import Int64, String
 from rclpy.qos import QoSProfile
 
-LIN_VEL_STEP_SIZE = 10
-ANG_VEL_STEP_SIZE = 10
-
-max_lin_vel = 250
-min_lin_vel = 0
-
-max_ang_vel = 250
-min_ang_vel = -250
-
 msg = """
 Control
 ---------------------------
@@ -40,8 +31,6 @@ e = """
 Communications Failed
 """
 
-def constrain(value, min_value, max_value):
-    return max(min(value, max_value), min_value)
 
 def get_key(settings):
     tty.setraw(sys.stdin.fileno())
@@ -54,26 +43,6 @@ def get_key(settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-def print_vels(target_linear_velocity, target_angular_velocity):
-    print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
-        target_linear_velocity,
-        target_angular_velocity))
-
-def make_simple_profile(output, input, slop):
-    if input > output:
-        output = min(input, output + slop)
-    elif input < output:
-        output = max(input, output - slop)
-    else:
-        output = input
-    return output
-
-def check_linear_limit_velocity(velocity):
-    return constrain(velocity, min_lin_vel, max_lin_vel)
-
-def check_angular_limit_velocity(velocity):
-    return constrain(velocity, min_ang_vel, max_ang_vel)
-
 def main():
     print(msg)
     settings = termios.tcgetattr(sys.stdin)
@@ -82,16 +51,8 @@ def main():
 
     qos = QoSProfile(depth=10)
     node = rclpy.create_node('teleop_keyboard')
-    #pub_linear = node.create_publisher(Int64, 'linear', qos)
-    #pub_angular = node.create_publisher(Int64, 'angular', qos)
     pub_key = node.create_publisher(String, 'key', qos)
 
-    '''
-    target_linear_velocity = 0.0
-    target_angular_velocity = 0.0
-    control_linear_velocity = 0.0
-    control_angular_velocity = 0.0
-    '''
     key_input = ' \n'
     try:
         while(1):
@@ -100,55 +61,21 @@ def main():
             if key == 'w':
                 key_input= 'w\n'
                 print(key_input)
-                #target_linear_velocity = check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
-                #print_vels(target_linear_velocity, target_angular_velocity)
             elif key == 's':
                 key_input= 's\n'
                 print(key_input)
-                #target_linear_velocity = check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
-                #print_vels(target_linear_velocity, target_angular_velocity)
             elif key == 'a':
                 key_input= 'a\n'
                 print(key_input)
-                #target_angular_velocity = check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
-                #print_vels(target_linear_velocity, target_angular_velocity)
             elif key == 'd':
                 key_input= 'd\n'
                 print(key_input)
-                #target_angular_velocity = check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
-                #print_vels(target_linear_velocity, target_angular_velocity)
-
             elif key == ' ' or key == 'p':
                 key_input= ' \n'
                 print(key_input)
-                #target_linear_velocity = 0.0
-                #control_linear_velocity = 0.0
-                #target_angular_velocity = 0.0
-                #control_angular_velocity = 0.0
-                #print_vels(target_linear_velocity, target_angular_velocity)
-
             else:
                 if (key == '\x03'):
                     break
-
-            '''
-            control_linear_velocity = make_simple_profile(
-                control_linear_velocity,
-                target_linear_velocity,
-                (LIN_VEL_STEP_SIZE / 2.0))
-
-            control_angular_velocity = make_simple_profile(
-                control_angular_velocity,
-                target_angular_velocity,
-                (ANG_VEL_STEP_SIZE / 2.0))
-
-            linear_msg = Int64()
-            linear_msg.data = int(target_linear_velocity)
-            pub_linear.publish(linear_msg)
-
-            angular_msg = Int64()
-            angular_msg.data = int(target_angular_velocity)
-            '''
             key_msg = String()
             key_msg.data = key_input
             #print(key_msg)
@@ -158,20 +85,12 @@ def main():
         print(e)
 
     finally:
-        #linear_msg = Int64()
-        #linear_msg.data = 0
-        #pub_linear.publish(linear_msg)
-
-        #angular_msg = Int64()
-        #angular_msg.data = 0
-        #pub_angular.publish(angular_msg)
         key_msg = String()
         key_msg.data = ' \n'
         #print(key_msg)
         pub_key.publish(key_msg)
 
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-
 
 if __name__ == '__main__':#
     main()
